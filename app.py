a257cdffc04d64f18da61763b179acab1cb56f4f
Abhijay <abhijay9@ymail.com>
2021-05-03 01:16:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
-from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
 
-run_with_ngrok(app)
 
 @app.route("/")
 def home():
@@ -16,6 +14,6 @@ def about():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
 
 
