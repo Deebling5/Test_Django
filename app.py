6a2edc2547ace668bdedaf75acdc7e4ef676af6f
Abhijay <abhijay9@ymail.com>
2021-05-03 03:03:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 from helium import *
 from selenium import webdriver
+from flask_ngrok import run_with_ngrok
+
 app = Flask(__name__)
+run_with_ngrok(app)
 
 
 @app.route("/")
